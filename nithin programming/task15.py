e8f03e570315185df57e70061260b540cb38d57b
24nadaneni-svg <24nadaneni@hazelwick.org>
2026-09-22 08:52:48 +0000

    22/09/26

---
+++ nithin programming/task15.py
@@ -0,0 +1,5 @@
+length = int(input("Enter your length in centimetres"))
+width = int(input("Enter your width in centimetres"))
+
+perimeter = length + width + length + width
+print("the perimeter is", ,"centimetres")
